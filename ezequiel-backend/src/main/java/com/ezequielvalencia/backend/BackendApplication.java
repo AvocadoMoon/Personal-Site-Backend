f4d25de007e18037c0e8081de2c1a5c54af1949f
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-03-25 21:08:07 -0400

    Add Cors Configuration

--- ezequiel-backend/src/main/java/com/ezequielvalencia/backend/BackendApplication.java
+++ ezequiel-backend/src/main/java/com/ezequielvalencia/backend/BackendApplication.java
@@ -4,10 +4,16 @@ import com.ezequielvalencia.backend.db.DBHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -17,8 +23,21 @@ public class BackendApplication {
 		ApplicationContext applicationContext = SpringApplication.run(BackendApplication.class, args);
 
 		// Automatically created from the settings within the 'application properties file'
-//		DBHandler dbHandler = new DBHandler(applicationContext.getBean(JdbcTemplate.class));
-//		dbHandler.createTables();
+//
+		DBHandler dbHandler = new DBHandler(applicationContext.getBean(JdbcTemplate.class));
+		dbHandler.createTables();
+	}
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+		// Don't do this in production, use a proper list  of allowed origins
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Access-Control-Allow-Origin"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 //	@Bean
 //	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
