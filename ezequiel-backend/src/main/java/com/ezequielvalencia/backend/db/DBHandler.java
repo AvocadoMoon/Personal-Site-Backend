b32ad27e11ac3b6995ba6a42aa4aa1f770bd1a25
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-03-29 14:26:26 -0400

    Clean Out Dead Code

--- ezequiel-backend/src/main/java/com/ezequielvalencia/backend/db/DBHandler.java
+++ ezequiel-backend/src/main/java/com/ezequielvalencia/backend/db/DBHandler.java
@@ -16,20 +16,10 @@ import java.nio.file.Path;
 @Component
 public class DBHandler {
     private final static Logger logger = LogManager.getLogger();
-    private final static String createGeoCacheTable = "CREATE TABLE IF NOT EXISTS geo_cache(" +
-            "id INTEGER NOT NULL PRIMARY KEY" +
-            "username TEXT NOT NULL" +
-            "note TEXT NOT NULL" +
-            "secret TEXT" +
-            "date TIMESTAMP NOT NULL" +
-            ")";
-
-    private final static String[] tablesToCreate = {createGeoCacheTable};
 
     @Autowired
     private JdbcTemplate template;
 
-//    @Autowired
     public DBHandler(JdbcTemplate dataSource){
         template = dataSource;
     }
@@ -43,90 +33,5 @@ public class DBHandler {
         template.execute(sql);
         logger.info("Created Tables");
     }
-
-
-//    @Value("${spring.datasource.url}")
-//    private static String dbURL;
-//    @Value("${spring.datasource.password}")
-//    private static String dbPassword;
-//    @Value("${spring.datasource.username}")
-//    private static String dbUsername;
-
-//    @Bean
-//    public static DataSource createDataSource(){
-//        String propertiesPath = Objects.requireNonNull(DBHandler.class.getClassLoader().getResource("hikari.properties")).getFile();
-//        System.out.println(propertiesPath);
-//        HikariConfig hikariConfig = new HikariConfig();
-//        hikariConfig.setConnectionTimeout(20000);
-//        hikariConfig.setMaxLifetime(1800000);
-//        hikariConfig.setPoolName("MainPool");
-//        hikariConfig.setIdleTimeout(30000);
-//        hikariConfig.setMaximumPoolSize(20);
-//        hikariConfig.setUsername(dbUsername);
-//        hikariConfig.setPassword(dbPassword);
-//        hikariConfig.setJdbcUrl(dbURL);
-//        System.out.println(hikariConfig.getJdbcUrl());
-//        return new HikariDataSource(hikariConfig);
-//    }
-
-//    private Properties hibernateProperties() {
-//        Properties properties = new Properties();
-//        properties.setProperty("hibernate.dialect", "postgres");
-//        logger.info("Hibernate properties created");
-//        return properties;
-//    }
-//
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactory() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//        sessionFactory.setDataSource(createDataSource());
-//        sessionFactory.setPackagesToScan("com.spring.boot.learning"); // Replace with your package name
-//        sessionFactory.setHibernateProperties(hibernateProperties());
-//        logger.info("session factory created");
-//        return sessionFactory;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-//        transactionManager.setSessionFactory(sessionFactory);
-//        return transactionManager;
-//    }
-
-
-//    public DBHandler(){
-//        createDataSource();
-//    }
-
-
-//    public LocalSessionFactoryBean sessionFactory(){
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//
-//        sessionFactory.setDataSource(createDataSource());
-//        sessionFactory.setPackagesToScan("com.spring.boot.learning"); // Replace with your package name
-//        sessionFactory.setHibernateProperties(hibernateProperties());
-//        logger.info("session factory created");
-//        return sessionFactory;
-//    }
-
-//    protected void initializeDB(){
-//
-//        try (Connection conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-//
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setString(1, text);
-//            int rowsInserted = pstmt.executeUpdate();
-//
-//            if (rowsInserted > 0) {
-//                System.out.println("Text inserted successfully!");
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 
 }
